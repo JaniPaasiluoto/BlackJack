6c38d1a976286e7216b28a66994c6fb556500288
JaniPaasiluoto <43338681+JaniPaasiluoto@users.noreply.github.com>
2018-10-11 10:26:25 +0300

    Add files via upload

---
+++ BlacJack/src/blacjack/Arvo.java
@@ -0,0 +1,8 @@
+
+package blacjack;
+
+
+public enum Arvo {
+     KAKSI, KOLME, NELJA, VIISI, KUUSI, SEITSEMAN, KAHDEKSAN, YHDEKSAN, KYMMENEN, JATKA, KUNINGATAR, KUNINGAS, ASSA
+}
+
---
+++ BlacJack/src/blacjack/Maa.java
@@ -0,0 +1,7 @@
+
+package blacjack;
+
+
+public enum Maa {
+    HERTTA, RUUTU, RISTI, PATA
+}
